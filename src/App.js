47def59ba38d5291218686a9e9dbbb07e5fcae1b
Gustavo Souza <GustavoSouzaAlves12@gmail.com>
2020-09-24 16:10:19 -0300

    MovieRow component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React, { useEffect, useState} from 'react'
+import Tmdb from './Tmdb'
+import MovieRow from './components/MovieRow'
+import "./App.css"
 export default () => {
+    const [movieList, setMovieList] = useState([])
+
+    useEffect(()=>{
+        const loadAll = async () => {
+            let showList = await Tmdb.getHomeList()
+            setMovieList(showList)
+
+        }
+        loadAll()
+    }, [])
+
     return (
-        <div>
-            OIII
+        <div className="page">
+            <section className="lists">
+                {movieList.map((item, key)=>(
+                    <MovieRow key={key} title={item.title} items={item.items}/>
+                ))}
+            </section>
         </div>
     )
 }
