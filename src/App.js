982b168352bfd96e8a6f44f0f070d999179815c0
Gustavo Souza <GustavoSouzaAlves12@gmail.com>
2020-09-26 16:56:51 -0300

    FeaturedMovie component created

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { useEffect, useState} from 'react'
+import "./App.css"
 import Tmdb from './Tmdb'
 import MovieRow from './components/MovieRow'
-import "./App.css"
+import FeaturedMovie from './components/FeaturedMovie'
+
 export default () => {
     const [movieList, setMovieList] = useState([])
+    const [FeaturedData, setFeaturedData] = useState([])
 
     useEffect(()=>{
         const loadAll = async () => {
             let showList = await Tmdb.getHomeList()
             setMovieList(showList)
 
+
+            
         }
         loadAll()
     }, [])
 
     return (
         <div className="page">
+
+            {FeaturedData &&
+                <FeaturedMovie item={FeaturedData}></FeaturedMovie>
+            }
+
             <section className="lists">
                 {movieList.map((item, key)=>(
                     <MovieRow key={key} title={item.title} items={item.items}/>
