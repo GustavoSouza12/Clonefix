b5b954d6038d1e8ec345b4c9d3001cbd6cd515dd
Gustavo Souza <GustavoSouzaAlves12@gmail.com>
2020-09-29 20:36:08 -0300

    folder components organization improved and header done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState} from 'react'
 import "./App.css"
 import Tmdb from './Tmdb'
-import MovieRow from './components/MovieRow'
-import FeaturedMovie from './components/FeaturedMovie'
-import Header from './components/Header'
+import MovieRow from './components/MovieRow/MovieRow'
+import FeaturedMovie from './components//FeaturedMovie/FeaturedMovie'
+import Header from './components/Header/Header'
+
 export default () => {
     const [movieList, setMovieList] = useState([])
     const [FeaturedData, setFeaturedData] = useState([null])
+    const [blackHeader, setBlackHeader] = useState(false)
 
     useEffect(()=>{
         const loadAll = async () => {
@@ -23,10 +25,25 @@ export default () => {
         loadAll()
     }, [])
 
+    useEffect(()=>{
+        const scrollListener = () => {
+            if(window.scrollY > 10){
+                setBlackHeader(true)
+            }else{
+                setBlackHeader(false)
+            }
+        }    
+            window.addEventListener('scroll', scrollListener)
+
+            return () =>{
+                window.removeEventListener('scroll', scrollListener)
+            }
+    }, [])
+
     return (
         <div className="page">
 
-            <Header />
+            <Header black={blackHeader}/>
 
             {FeaturedData &&
                 <FeaturedMovie item={FeaturedData}></FeaturedMovie>
